9cf3a42e698f670c4599e7374d6426587017ee7c
oleg.nikolaev <oleg.nikolaev@mango.rocks>
2021-12-27 18:19:31 +0300

    Add class Calculator. Add unit tests.

--- src/test/java/com/sbrf/reboot/calculator/CalculatorTest.java
+++ src/test/java/com/sbrf/reboot/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.sbrf.reboot.calculator;
 
+import com.sbrf.reboot.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,5 +30,20 @@ class CalculatorTest {
     @Test
     void classHasSevenMethods() {
         assertEquals(7, Calculator.class.getMethods().length - Object.class.getMethods().length);
+    }
+
+    @Test
+    void getSystemInfo() {
+        assertEquals("Calculator v1", new Calculator().getSystemInfo());
+    }
+
+    @Test
+    void getPow() {
+        assertEquals(16, new Calculator().getPow(2, 4));
+    }
+
+    @Test
+    void getAbs() {
+        assertEquals(4, new Calculator().getAbs(-4));
     }
 }
