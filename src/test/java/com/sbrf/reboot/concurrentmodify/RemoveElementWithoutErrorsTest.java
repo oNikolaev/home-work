6d540d4116678b9b3efe11ec9409e9a8f44b719a
oleg.nikolaev <oleg.nikolaev@mango.rocks>
2022-01-31 01:36:16 +0300

    Homework 8. Correct procedure successConcurrentModificationException() - fix bug. Add successRemoveElementNoException() - simple remove  from list. Add successRemoveElementNoExceptionUseIterator() - remove from list with iterator.

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -1,37 +1,60 @@
 package com.sbrf.reboot.concurrentmodify;
 
+import com.sun.java.swing.plaf.windows.WindowsDesktopIconUI;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
+    private List<Integer> list;
+    final int indexToRemove = 1;
 
-    @Test
-    public void successConcurrentModificationException() {
-
-        List<Integer> list = new ArrayList() {{
+    @BeforeEach
+    public void beforeEachTest() {
+        list = new ArrayList() {{
             add(1);
             add(2);
             add(3);
         }};
+    }
 
+    @Test
+    public void successConcurrentModificationException() {
         assertThrows(ConcurrentModificationException.class, () -> {
-            
             for (Integer integer : list) {
-                list.remove(1);
+                list.remove(indexToRemove);
             }
-            
         });
 
     }
 
     @Test
-    public void successRemoveElementWithoutErrors() {
+    public void successRemoveElementNoException() {
+        assertDoesNotThrow(() -> {
+            list.remove(indexToRemove);
+        });
+    }
 
+    @Test
+    public void successRemoveElementNoExceptionUseIterator() {
+        assertDoesNotThrow(() -> {
+            int index = 0;
+            for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext(); index++) {
+                iterator.next();
+                if (index == indexToRemove) {
+                    iterator.remove();
+                }
+            }
+        });
     }
 
 }
